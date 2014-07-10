8db9b38a9bc3a77c88d18360a06ac53548a1a486
anita-chang <anita@ecrowdmedia.com>
2014-07-10 18:06:13 +0800

    建立head樣版

---
+++ application/views/templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $title ?></title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" />
+</head>
+
+<body>
+	<div class="wrap">
+		<div class="top">
+		</div>
