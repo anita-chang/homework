d487946ce2c6319d8c504553c21d46ab67473fea
anita-chang <anita@ecrowdmedia.com>
2014-07-16 17:47:35 +0800

    修改create product相關頁面呈現樣式

--- application/views/create_prd.php
+++ application/views/create_prd.php
@@ -1,21 +1,15 @@
 <h2>新增產品</h2>
-
-<?php echo validation_errors(); ?>
-
+<div class="error">
+	<?php echo validation_errors(); ?>
+</div>
 <?php echo form_open('create') ?>
-
-	<label for="pname">產品名稱</label> 
-	<input type="input" name="pname" /><br />
-
-	<label for="pinfo">產品簡介</label>
-	<textarea name="pinfo"></textarea><br />
-
-	<label for="pdes">產品說明</label>
-	<textarea name="pdes"></textarea><br />
-
-	<label for="pprice">產品價格</label> 
-	<input type="input" name="pprice" /><br />
-
-	<input type="submit" name="submit" value="增加新產品" /> 
-
+	<ul>
+		<li>產品名稱：<input type="input" name="pname" /></li>
+		<li>產品簡介：<textarea name="pinfo"></textarea></li>
+		<li>產品說明：<textarea name="pdes"></textarea></li>
+		<li>產品價格：<input type="input" name="pprice" /></li>
+	</ul>
+	<div class="">
+		<input type="submit" name="submit" value="增加新產品" /> 
+	</div>
 </form>
--- application/views/create_success.php
+++ application/views/create_success.php
@@ -1,2 +1,4 @@
-<p>恭喜!!產品新增成功!</p>
-<p><a href="<?php echo site_url() ;?>/view_all" title="">回去看看產品吧!</a></p>
+<div class="success">
+	<p>恭喜!!產品新增成功!</p>
+	<p><a href="<?php echo site_url() ;?>/view_all" title="產品總覽">回去看看產品吧!</a></p>
+</div>
