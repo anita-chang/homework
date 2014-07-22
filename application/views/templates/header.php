a1d9a21b6bc5441d27a3a96c308d70e12f57d3b5
anita-chang <anita@ecrowdmedia.com>
2014-07-22 17:41:56 +0800

    新增banner

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,12 +7,12 @@
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/reset.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/all.css" />
+
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="<?php echo base_url();?>js/jquery.confirm.js"></script>
 <script src="<?php echo base_url();?>bootstrap/js/bootstrap.min.js"></script>
 <link href="<?php echo base_url();?>bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/all.css" />
 </head>
 
 <body>
@@ -29,5 +29,38 @@
 			</div>
 		</div>
 		<div class="clear"></div>
-		<div class="banner"><img src="<?php echo base_url();?>image/banner.png" alt="banner" /></div>
-		<div class="content">
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="<?php echo base_url();?>image/banner1.png" alt="">
+    </div>
+    <div class="item">
+      <img src="<?php echo base_url();?>image/banner2.png" alt="">
+    </div>
+    <div class="item">
+      <img src="<?php echo base_url();?>image/banner3.png" alt="">
+    </div>
+    <div class="item">
+      <img src="<?php echo base_url();?>image/banner4.png" alt="">
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+
+<div class="content">
