8e94cfdef2e432ae25d3194bebe4a83ed939d62f
anita-chang <anita@ecrowdmedia.com>
2014-07-29 12:18:49 +0800

    修改樣式

--- application/views/product_all.php
+++ application/views/product_all.php
@@ -1,6 +1,6 @@
 
 <?php foreach ($prds as $prds_item){ ?>
-		<div class="main">
+		<div class="main form-control">
 			<div class="con_img"><a href="<?php echo site_url().'/'.$prds_item['pid'];?>" title="<?php echo $prds_item['pname'] ?>"><img src="<?php echo base_url();?>image/<?php echo $prds_item['pimg'] ?>" alt="<?php echo $prds_item['pname'] ?>" /></a></div>
 			<p class="name"><a href="<?php echo site_url().'/'.$prds_item['pid'];?>" title="<?php echo $prds_item['pname'] ?>"><?php echo $prds_item['pname'] ?></a></p>
 			<p class="des"><?php echo $prds_item['pinfo'] ?></p>
